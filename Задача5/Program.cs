5c2ccc186e1a74fa2fb7dbad7b81d8c45fce7ece
Reseda <Reseda@examle.com>
2022-09-18 21:03:07 +0400

    Добавили задачу 5 и задачу 7

---
+++ Задача5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу которая на вход принимает одно число N, а на выходе показывает все целые числа от -N до N
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+if(n<0)
+    {
+        n=-n;
+    }
+int b = -n;
+while (b<=n)
+    {
+        Console.WriteLine($"{b}");
+        b++;
+    }
