1899d15ebe8a1518de6b8e0eefde0fbd60b462b0
Reseda <Reseda@examle.com>
2022-09-18 19:52:10 +0400

    Добавили задачи 1_1 и 3

---
+++ Задача1_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу котрая на вход принимает 2 числа, и проверяет, является ли первое число квадратом второго.
+Console.Clear();
+Console.WriteLine("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 == number2 * number2)
+    {
+        Console.WriteLine($"Число {number1} является квадратом числа {number2}");
+    }
+else
+    {
+    Console.WriteLine($"Число {number1} НЕ является квадратом числа {number2}");
+    }
---
+++ Задача3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Программа, которая будет выдавать название дня недели по заданному номеру
+Console.Clear();
+Console.WriteLine("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine());
+if (number > 7||number < 1)
+    {
+        Console.WriteLine("В неделе только 7 дней");
+        return;
+    }
+if (number == 1)
+    {
+        Console.WriteLine($"День {number} это Понедельник");
+    }
+if (number == 2)
+    {
+        Console.WriteLine($"День {number} это Вторник");
+    }
+if (number == 3)
+    {
+        Console.WriteLine($"День {number} это Среда");
+    }
+if (number == 4)
+    {
+        Console.WriteLine($"День {number} это Четверг");
+    }
+if (number == 5)
+    {
+        Console.WriteLine($"День {number} это Пятница");
+    }
+if (number == 6)
+    {
+        Console.WriteLine($"День {number} это Суббота");
+    }
+if (number ==7)
+    {
+        Console.WriteLine($"День {number} это Воскресенье");
+    }
