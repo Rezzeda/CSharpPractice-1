41f47f9140adcc6933ff1184e4d1ac345e82499a
Reseda <Reseda@examle.com>
2022-09-18 18:32:13 +0400

    добавили задачу 1

---
+++ Задача1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Ввести число и вывести его квадрат
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
+int sqr1 = Convert.ToInt32 (Math.Pow(number, 2)); // здесь может быть любая степень
+Console.WriteLine($"Квадрат числа {number} равен {sqr1}");
