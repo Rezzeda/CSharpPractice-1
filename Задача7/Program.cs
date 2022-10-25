9bef86333e6d3646231657843ed84233db4095e2
Reseda <catdog3556@yandex.ru>
2022-10-25 19:25:39 +0400

    add 1seminar csharp

---
+++ Задача7/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа. которая на вход принимает трехзначное число. на выход показывает последнюю цифру этого числа
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n < 100||n > 999)
+    {
+        Console.WriteLine("Введено не трехзначное число");
+    }
+else
+    {
+        int lastdigit = n%10;
+        Console.WriteLine($"Последняя цифра: {lastdigit} ");
+    }
