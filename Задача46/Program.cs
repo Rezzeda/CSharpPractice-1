be434689353f6bce95b99e988e4d3a1f4edfa6fa
Reseda <catdog3556@yandex.ru>
2022-10-26 17:09:34 +0400

    add 7 seminar

---
+++ Задача46/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте двумерный массив размером (m x n) заполненный случайными целыми числами
+using System; // <- это чтоб не писать Console.
+using static System.Console; // <- это чтоб не писать Console.
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows=int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns=int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 9);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
