c0d1a4c7c0037d45b51b4ae170e6ab0408e71710
Reseda <catdog3556@yandex.ru>
2022-10-26 11:47:30 +0400

    add 2 seminar

---
+++ Задача11/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+Console.Clear();
+
+int num = new Random().Next(100,1000);
+
+int num1 = num/100;
+int num2 = num%100;
+int num3 = num%10;
+
+
+Console.WriteLine($"{num}");
+Console.WriteLine($"{num1}{num3}");
+
+
+
---
+++ Задача16/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, 
+//является ли одно число квадратом другого.
+Console.Clear();
+Console.WriteLine("Введите число 1: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 == num2*num2)
+{
+    Console.WriteLine($" Первое число является квадратом второго");
+    return;
+}
+
+if (num2 == num1*num1)
+{
+    Console.WriteLine($" Второе число является квадратом первого");
+    return;
+}
+
+else
+{
+    Console.WriteLine($"одно число НЕ является квадратом другого");
+}
