0d249fcdc11d193cbb8c84df5deb45ceec3deebc
Reseda <catdog3556@yandex.ru>
2022-10-26 15:04:09 +0400

    add 3 seminar homework

---
+++ Задача21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит 
+// расстояние между ними в 3D пространстве.
+// АВ=√(xb-xa)2 +(yb-ya)2 + (zb-za)2
+
+Console.Clear();
+Console.WriteLine("Введите координату X первой точки: ");
+double xa = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Y первой точки: ");
+double ya = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Z первой точки: ");
+double za = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату X второй точки: ");
+double xb = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Y второй точки: ");
+double yb = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Z второй точки: ");
+double zb = double.Parse(Console.ReadLine());
+
+double AB = Math.Sqrt(Math.Pow((xb-xa),2)+Math.Pow((yb-ya),2)+Math.Pow((zb-za),2));
+
+Console.WriteLine($"{AB:f3}");
---
+++ Задача23/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+double n = double.Parse(Console.ReadLine());
+int i = 1;
+while(i<=n)
+{
+    double a = Math.Pow(i,3);
+    i++;
+    Console.Write($"{a} ");
+}
+
